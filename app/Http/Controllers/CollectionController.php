7606c3b6fab92df9adb91f1fdbcdb32e60083300
MohammedHassona99 <mohammedhassona99@hotmail.com>
2023-08-14 13:58:24 +0300

    collection-filter

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -12,10 +12,23 @@ class CollectionController extends Controller
         // $arr = [1, 2, 3, 4];
         $arr = ['name', 'age'];
         $collect = collect($arr);
-         $collect->combine(['mohammed', '16']);
-         $collect->count();
+        $collect->combine(['mohammed', '16']);
+        $collect->count();
 
-         $categories = Category::get();
+        $categories = Category::get();
+        // return $categories;
 
+        // remove
+        // add
+        // return $categories->each(function ($category) {
+        //     $category->name = 'mohammed';
+        //     unset($category->title);
+        //     unset($category->description);
+        //     return $category;
+        // });
+        $categories->collect($categories);
+        return $categories->transform(function ($value) {
+            return 'title are : ' . $value['title'];
+        });
     }
 }
